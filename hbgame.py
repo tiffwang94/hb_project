8df9d451eb83edd484471b6dc474c65988c84a06
Tiffany wang <tiffanywang@Tiffanys-MacBook-Air.local>
2017-07-09 12:13:10 -0700

    added a basic grid

--- hbgame.py
+++ hbgame.py
@@ -21,7 +21,39 @@ print "Hello, {}!".format(playertwo)
 print
 print "The object of this game is to connect four letters in a row, either diagonally, horizontally, or vertically."
 print "Each player must choose their coordinates, first beginning from the bottom and make their way up the board."
+print "coordinates will be chosen beginning with a number on the left and then a number on the bottom."
 print
 print "{}, you will be using the letter 'X' and {}, you will be using the letter 'O'".format(playerone, playertwo)
+print
+
+row_5 = ["E", "E", "E", "E", "E"]
+row_4 = ["D", "D", "D", "D", "D"]
+row_3 = ["C", "C", "C", "C", "C"]
+row_2 = ["B", "B", "B", "B", "B"]
+row_1 = ["A", "A", "A", "A", "A"]
+
+
+print "5 {}".format(row_5)
+print "---"
+print "4 {}".format(row_4)
+print "---"
+print "3 {}".format(row_3)
+print "---"
+print "2 {}".format(row_2)
+print "---"
+print "1 {}".format(row_1)
+print "----1 -- 2 -- 3 -- 4 -- 5"
 print 
 print "{}, you will be starting first, please choose your first coordinates.".format(playerone)
+
+
+
+
+
+
+
+
+
+
+
+
